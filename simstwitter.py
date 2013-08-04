288a95cec0e782ce8251e1d82372d94846192357
Kayra <KayraAlat@gmail.com>
2013-08-04 19:07:03 +0100

    Prank messages added

--- simstwitter.py
+++ simstwitter.py
@@ -1,16 +1,34 @@
 from twitter import *
 import simplejson as json
 
+
 twitter = Twitter(
             auth=OAuth('41864427-rQTnd0gZZb1w1J1zGsLyNGxdP6Jq7Mm3HGKumB8Ll', '5zIesDuQuQbEdx88E2nrZtuiTIDMpSV80SzGKHjEalQ',
                       'sTYfrPfIZP1OFfTqz0m0Q', 'lCMKOz1iJXlmTEuYnx2dAYuucbi9VceuRAxomCJl5vY')
            )
 
+messages = ["Your energy use has just spiked. You'd better be careful.", 
+	         "Do you canoe?", 
+	         "You have been chosen. They will come soon.", 
+	         "You're a Winter and should decorate accordingly if you want to live most harmoniously in your house.", 
+	         "The number 3 is very important in your life right now.", 
+	         "Your psychic advisor suggests that you work on improving relationships with those closest to you; you'll need them in the next lunar review.", 
+	         "Your psychic advisor has had strong vibrations coming from your Sixth House. Stay away from any building activities during the next week.", 
+	         "Your psychic advisor suggests that you keep your secrets well this month. There may be untrustworthy individuals in your environment.", 
+	         "The drop off has been made. You've been warned.", 
+	         "Your psychic advisor suggests that you plan your meetings very carefully this month as you may have some unexpected news.", 
+	         "The number 6 will be very important for you in the next 24 hours.", 
+	         "The end is near. Make preparations.", 
+	         "The flashing light was just a test. You'll have plenty of warning next time.", 
+	         "They're coming soon. Maybe you should think twice about opening the door.", 
+	         "They're fixing your phone line. Don't pick up the phone the next time it rings."]
+
 def postTweet(message):
     log = open('log', 'a')
-    log.write("\nMessage being tweeted: %s" % message)
+    log.write("\nMessage being tweeted: %s \n" % message)
     print "Message being tweeted: %s" % message
     twitter.statuses.update(status=message)
     log.close()
 
-postTweet("Test tweet gurl")
+postTweet(messages[0])
+
